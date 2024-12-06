b2abbc49580b98391d668a475797423e4b3d3549
rizkon86system <rizkon86system@gmail.com>
2024-12-07 06:28:20 +0700

    Menambahkan konsol

--- index.js
+++ index.js
@@ -1,42 +1,5 @@
-// //import express
-// const express = require("express");
-
-// //import CORS
-// const cors = require("cors");
-
-// //import bodyParser
-// const bodyParser = require("body-parser");
-
-// //import router
-// const router = require("./routes/articleRoutes");
-
-// //init app
-// const app = express();
-
-// //use cors
-// app.use(cors());
-
-// //use body parser
-// app.use(bodyParser.urlencoded({ extended: false }));
-
-// // parse application/json
-// app.use(bodyParser.json());
-
-// //define port
-// const port = 3000;
-
-// //route
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
-
-// //define routes
-// app.use("/api", router);
-
-// //start server
-// app.listen(port, () => {
-//   console.log(`Server started on port ${port}`);
-// });
+// Menampilkan nilai DATABASE_URL di console untuk debugging
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
 
 // Import express, multer, path, dan prisma
 const express = require("express");
@@ -45,9 +8,6 @@ const bodyParser = require("body-parser");
 const multer = require("multer");
 const path = require("path");
 const prisma = require("./prisma/client"); // Pastikan prisma client sudah benar di-import
-
-// Menampilkan nilai DATABASE_URL di console untuk debugging
-console.log("DATABASE_URL:", process.env.DATABASE_URL);
 
 // Setup storage engine untuk multer
 const storage = multer.diskStorage({
